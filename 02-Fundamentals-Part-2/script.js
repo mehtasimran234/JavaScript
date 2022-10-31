1e1f1c9658ef0ab1f018304c00bc02630d61c252
mehtasimran234 <simran.mehta@zeuslearning.com>
2022-10-31 12:00:45 +0530

    Section 3: Coding challenge 4

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -87,27 +87,74 @@ GOOD LUCK 😀
 */
 
 
-const mark = {
-    fullName: "Mark Miller", 
-    weight: 78,
-    height: 1.69,
-    calcBMI: function() {
-        this.bmi = this.weight / this.height ** 2;
-        return this.bmi;
-    }
-}
-const john = {
-    fullName: "John Smith", 
-    weight: 92,
-    height: 1.95,
-    calcBMI: function() {
-        this.bmi = this.weight / this.height ** 2;
-        return this.bmi;
-    }
+// const mark = {
+//     fullName: "Mark Miller", 
+//     weight: 78,
+//     height: 1.69,
+//     calcBMI: function() {
+//         this.bmi = this.weight / this.height ** 2;
+//         return this.bmi;
+//     }
+// }
+// const john = {
+//     fullName: "John Smith", 
+//     weight: 92,
+//     height: 1.95,
+//     calcBMI: function() {
+//         this.bmi = this.weight / this.height ** 2;
+//         return this.bmi;
+//     }
+// }
+
+// if (mark.calcBMI() > john.calcBMI()) {
+//     console.log(`${mark.fullName}'s BMI (${mark.bmi}) is higher than ${john.fullName}'s (${john.bmi})!`);
+// } else if (john.bmi > mark.bmi) {
+//     console.log(`${john.fullName}'s BMI (${john.bmi}) is higher than ${mark.fullName}'s (${mark.bmi})!`);
+// }
+
+
+
+///////////////////////////////////////
+// Coding Challenge #4
+
+/*
+Let's improve Steven's tip calculator even more, this time using loops!
+
+1. Create an array 'bills' containing all 10 test bill values
+2. Create empty arrays for the tips and the totals ('tips' and 'totals')
+3. Use the 'calcTip' function we wrote before (no need to repeat) to calculate tips and total values (bill + tip) for every bill value in the bills array. Use a for loop to perform the 10 calculations!
+
+TEST DATA: 22, 295, 176, 440, 37, 105, 10, 1100, 86 and 52
+
+HINT: Call calcTip in the loop and use the push method to add values to the tips and totals arrays 😉
+
+4. BONUS: Write a function 'calcAverage' which takes an array called 'arr' as an argument. This function calculates the average of all numbers in the given array. This is a DIFFICULT challenge (we haven't done this before)! Here is how to solve it:
+  4.1. First, you will need to add up all values in the array. To do the addition, start by creating a variable 'sum' that starts at 0. Then loop over the array using a for loop. In each iteration, add the current value to the 'sum' variable. This way, by the end of the loop, you have all values added together
+  4.2. To calculate the average, divide the sum you calculated before by the length of the array (because that's the number of elements)
+  4.3. Call the function with the 'totals' array
+
+GOOD LUCK 😀
+*/
+
+const calcTip = bill => (bill >= 50 && bill <= 300) ? bill * 0.15 : bill * 0.2;
+
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+
+const tips = [];
+const totals = [];
+
+for (let i = 0; i < bills.length; i++) {
+    tips.push(calcTip(bills[i]));
+    totals.push(bills[i] + tips[i]);
 }
 
-if (mark.calcBMI() > john.calcBMI()) {
-    console.log(`${mark.fullName}'s BMI (${mark.bmi}) is higher than ${john.fullName}'s (${john.bmi})!`);
-} else if (john.bmi > mark.bmi) {
-    console.log(`${john.fullName}'s BMI (${john.bmi}) is higher than ${mark.fullName}'s (${mark.bmi})!`);
+const calcAverage = function (array) {
+    let sum = 0;
+    for(let i = 0; i < array.length; i++) {
+        sum = sum + array[i];
+    }
+    return sum / array.length;
 }
+
+console.log(bills, tips, totals);
+console.log(calcAverage(bills));
