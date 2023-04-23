c4a986838e16eda6b221589f4d6854326e3132e2
mehtasimran234 <simran.mehta@zeuslearning.com>
2023-04-23 15:01:17 +0530

    Section 7: Project#1 Guess My Number - Coding Challenge 1

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -1,1 +1,65 @@
 "use strict";
+
+///////////////////////////////////////
+// Coding Challenge #1
+
+/* 
+Implement a game rest functionality, so that the player can make a new guess! Here is how:
+
+1. Select the element with the 'again' class and attach a click event handler
+2. In the handler function, restore initial values of the score and secretNumber variables
+3. Restore the initial conditions of the message, number, score and guess input field
+4. Also restore the original background color (#222) and number width (15rem)
+
+GOOD LUCK 😀
+*/
+
+let secretNumber = Math.ceil(Math.random() * 20);
+let score = 20;
+
+document.querySelector(".again").addEventListener("click", () => {
+  score = 20;
+  secretNumber = Math.ceil(Math.random() * 20);
+
+  document.querySelector(".score").textContent = score;
+  document.querySelector(".guess").value = "";
+  document.querySelector(".message").textContent = "Start guessing...";
+  document.querySelector("body").style.backgroundColor = "#222";
+  document.querySelector(".number").style.width = "15rem";
+  document.querySelector(".number").textContent = "?";
+});
+
+document.querySelector(".check").addEventListener("click", () => {
+  const guessNumber = Number(document.querySelector(".guess").value);
+
+  if (!guessNumber) {
+    // When there is no input
+    document.querySelector(".message").textContent = "⛔ No number!";
+  } else if (guessNumber === secretNumber) {
+    // When player wins the game
+    document.querySelector(".message").textContent = "🎉 Correct number!";
+    document.querySelector("body").style.backgroundColor = "#60b347";
+    document.querySelector(".number").style.width = "30rem";
+    document.querySelector(".number").textContent = secretNumber;
+  } else if (guessNumber > secretNumber) {
+    // When the guess is too high
+    if (score > 1) {
+      document.querySelector(".message").textContent = "👆 Too high!";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "😔 You lost the game.";
+      document.querySelector(".score").textContent = 0;
+    }
+  } else if (guessNumber < secretNumber) {
+    // When the guess is too low
+    if (score > 1) {
+      document.querySelector(".message").textContent = "👇 Too low!";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "😔 You lost the game.";
+      document.querySelector(".score").textContent = 0;
+    }
+  }
+});
